ff80fc70cfa4db9f1b48e8367184cf753022ed81
ericksonnascimento <erickson.nascimento@movile.com>
2022-03-06 19:42:37 -0300

    feat: category routes, middlewares and main server

---
+++ src/main/server.ts
@@ -0,0 +1,8 @@
+import 'module-alias/register';
+import env from '@/main/config/env';
+
+(async () => {
+  const { setupApp } = await import('@/main/config/app');
+  const app = await setupApp();
+  app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}}`));
+})();
